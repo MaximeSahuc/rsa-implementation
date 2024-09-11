36a704469a3efc84be7f4b390a5fd01fb1dbe977
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-09-11 16:28:49 +0200

    add server

---
+++ server.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+#####################
+# Fichier de Maxime #
+#####################
+
+
+def main():
+    print("---- Server ----")
+
+
+if __name__ == "__main__":
+    main()
+
+
