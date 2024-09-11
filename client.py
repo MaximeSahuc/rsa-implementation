53e0fd778634f9d54364529d3457735c73e6a5f4
marccambon <160885185+marccambon@users.noreply.github.com>
2024-09-11 14:53:58 +0000

    gen message chiffré

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
+#info serveur
+N=551
+E=
 
+#Message
+M=89865
+
+#Calcul M^E
+Temp=M**E
+
+#Message chiffré(Reste de la division de Temp par N)
+Message_chiffré = Temp % N
+
+print("Message_chiffré")
